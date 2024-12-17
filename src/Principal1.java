7b17b7a3765303df8458fd67bc6335302158a293
Jean <jeanmatheus0612@gmail.com>
2024-12-17 12:24:44 -0300

    Criando o projeto e fazendo o primeiro exercicio de POO

---
+++ src/Principal1.java
@@ -0,0 +1,7 @@
+public class Principal1 {
+    public static void main(String[] args) throws Exception {
+        Pessoa mensagemPessoa = new Pessoa();
+
+        mensagemPessoa.exibeMensagem();
+    }
+}
