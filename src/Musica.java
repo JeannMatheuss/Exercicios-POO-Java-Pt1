dc9563777237cbefff2abbe5859538693992ea71
Jean <jeanmatheus0612@gmail.com>
2024-12-17 13:05:01 -0300

    Criando os metodos e finalizando o exercicio da musica

--- src/Musica.java
+++ src/Musica.java
@@ -1,3 +1,24 @@
 public class Musica {
+    public String titulo;
+    public String artista;
+    public int anoDeLancamento;
+    public double avaliacao;
+    public double somaAvaliacoes;
+    public int numAvaliacoes;
+
+    void fichaTecnica(){
+        System.out.println("Título: " + titulo);
+        System.out.println("Artista: " + artista);
+        System.out.println("Ano de Lançamento: " + anoDeLancamento);
+    }
+
+    void avalia(double avaliacao){
+        somaAvaliacoes += avaliacao;
+        numAvaliacoes++;
+    }
+
+    double mediaAvaliacoes(){
+        return somaAvaliacoes / numAvaliacoes;
+    }
     
 }
--- src/Principal3.java
+++ src/Principal3.java
@@ -1,9 +1,22 @@
 public class Principal3 {
-    public String titulo;
-    public String artista;
-    public int anoDeLancamento;
-    public double avaliacao;
-    public int numAvaliacoes;
+    public static void main(String[] args) {
+        Musica musica = new Musica();
 
+        musica.titulo = "Die With a Smile";
+        musica.artista = "Bruno Mars and Lady Gaga";
+        musica.anoDeLancamento = 2024;
 
+        // Exibe o método da ficha tecnica criada no arquivo Musica.java
+        musica.fichaTecnica();
+
+        // Notas para a soma, media e numeros de avaliações
+        musica.avalia(10);
+        musica.avalia(8);
+        musica.avalia(7);
+
+        // Print dos métodos
+        System.out.println(musica.somaAvaliacoes);
+        System.out.println(musica.numAvaliacoes);
+        System.out.println(musica.mediaAvaliacoes());
+    }
 }
