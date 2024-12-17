44dbb84e247e87e51f5de8cf4a0f416e4dd880da
Jean <jeanmatheus0612@gmail.com>
2024-12-17 13:36:12 -0300

    Projeto usando classes, metodos, criação de instancias usando Aluno.java

---
+++ src/Aluno.java
@@ -0,0 +1,10 @@
+public class Aluno {
+    String nome;
+    int idade;
+
+    void exibeDadosDoAluno(){
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+    }
+
+}
---
+++ src/Principal5.java
@@ -0,0 +1,10 @@
+public class Principal5 {
+    public static void main(String[] args) {
+        Aluno aluno = new Aluno();
+
+        aluno.nome = "Jean";
+        aluno.idade = 19;
+
+        aluno.exibeDadosDoAluno();
+    }
+}
